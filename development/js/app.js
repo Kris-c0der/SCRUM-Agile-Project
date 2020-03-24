0d760606fec45e2644a386e9d68f5165c9e93e93
Hubert-Kujawiak <h.kujawiak@hotmail.com>
2020-03-24 18:59:55 +0100

    zad-2.3 end

--- development/js/app.js
+++ development/js/app.js
@@ -2,14 +2,18 @@ const nameUser = document.querySelector('.icons');
 const form = document.querySelector('form');
 const inpText = document.querySelector('#value');
 const secForm = document.querySelector('.nav-welcome');
-console.log(secForm);
 
 form.addEventListener('submit', function (elem) {
     elem.preventDefault();
         nameUser.innerText = inpText.value;
         var userName = inpText.value;
         localStorage.setItem('savedName', userName);
-        console.log("Twoje imie zapisane w LS to:  ", localStorage.savedName );
+        console.log("Twoje imie zapisane w LS to:  ", localStorage.savedName);
         secForm.style.display = 'none';
 
 });
+
+if(localStorage.length > 0){
+    secForm.style.display = 'none';
+    nameUser.innerText = localStorage.savedName;
+}
