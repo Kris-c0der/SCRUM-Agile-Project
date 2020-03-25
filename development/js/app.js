240b4a79170228b03604a9152f3c735a40bc55d3
Hubert-Kujawiak <h.kujawiak@hotmail.com>
2020-03-25 18:33:41 +0100

    dodanie wlasciwosci display

--- development/js/app.js
+++ development/js/app.js
@@ -2,6 +2,8 @@ const nameUser = document.querySelector('.icons');
 const form = document.querySelector('form');
 const inpText = document.querySelector('#value');
 const secForm = document.querySelector('.nav-welcome');
+const mainSect = document.querySelector('.main-section');
+console.log(mainSect);
 
 form.addEventListener('submit', function (elem) {
     elem.preventDefault();
@@ -10,10 +12,12 @@ form.addEventListener('submit', function (elem) {
         localStorage.setItem('savedName', userName);
         console.log("Twoje imie zapisane w LS to:  ", localStorage.savedName);
         secForm.style.display = 'none';
+        mainSect.style.display = 'block';
 
 });
 
 if(localStorage.length > 0){
     secForm.style.display = 'none';
     nameUser.innerText = localStorage.savedName;
+
 }
