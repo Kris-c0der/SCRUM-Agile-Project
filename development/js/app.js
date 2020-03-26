c7dad23fe2c637bf27b5c33a8f4f179163db0906
Damian Karcz <damian.karcz95@tlen.pl>
2020-03-26 19:02:17 +0100

    4.2Logika - nowe przepisy

--- development/js/app.js
+++ development/js/app.js
@@ -41,3 +41,92 @@ btnAddPlan.addEventListener("click", function() {
   // dodać element który będzie ustawiał display block dla elementu plan //
   console.log("click");
 });
+
+
+///////////////////*New Recipes - Logic*///////////////////////
+const btnElIns = document.querySelector('.instruction-btn');
+console.log(btnElIns);
+const textAreaInst = document.querySelector('#instruction-input');
+console.log(textAreaInst);
+const olEl = document.querySelector('.instruction-list');
+console.log(olEl);
+let instructionAll = [];
+
+//Event dodanie instrukcji
+btnElIns.addEventListener('click',function (e) {
+  console.log('click');
+  e.preventDefault();
+  const textAreaInstValue = document.querySelector('#instruction-input').value;
+  if (textAreaInstValue.length < 150) {
+    const newLi = document.createElement('li');
+    olEl.appendChild(newLi);
+    newLi.innerText = textAreaInstValue;
+    instructionAll.push(textAreaInstValue);
+    textAreaInst.value = '';
+  } else {
+    alert('Maksymalna liczba znaków: 150');
+  }
+});
+
+//Event dodanie składnika
+const btnElIng = document.querySelector('.ingredients-btn');
+console.log(btnElIng);
+const ulEl = document.querySelector('.ingredients-list');
+console.log(ulEl);
+const inputIng = document.querySelector('#ingredients-input');
+console.log(inputIng);
+let ingrediendsAll = [];
+
+btnElIng.addEventListener('click',function (e) {
+  console.log('click');
+  e.preventDefault();
+  const inputIngValue = document.querySelector('#ingredients-input').value;
+  if (inputIngValue.length < 50) {
+    const newLi2 = document.createElement('li');
+    ulEl.appendChild(newLi2);
+    newLi2.innerText = inputIngValue;
+    ingrediendsAll.push(inputIngValue);
+    inputIng.value ='';
+  } else {
+    alert('Maksymalna liczba znaków: 50');
+  }
+});
+
+//Nazwa i opis przepisu
+const nameRecipInput = document.querySelector('#recipe-name');
+console.log(nameRecipInput);
+const decriptionInput = document.querySelector('#recipe-description');
+console.log(decriptionInput);
+const saveBtnEl = document.querySelector('#save-recipes-btn');
+console.log(saveBtnEl);
+const formEl = document.querySelector('#form-add-recipe');
+
+formEl.addEventListener('submit', function (element) {
+  console.log('submit');
+  let decriptionInputVal = document.querySelector('#recipe-description').value;
+  let nameRecipInputVal = document.querySelector('#recipe-name').value;
+  if (decriptionInputVal.length >50 && nameRecipInputVal.length >360){
+    element.preventDefault();
+    alert('Zbyt duża liczba znaków. Maksymlana liczba znaków dla "Nazwa przepisu": 50, dla "Opis Przepisu": 360.')
+  } else {
+    let recipeList = JSON.parse(localStorage.getItem("recipsList"));
+    if (recipeList === null) {
+      recipeList = [];
+    }
+    recipeList.push({description: decriptionInputVal, nameRecipe: nameRecipInputVal, instruction: instructionAll, ingrediends: ingrediendsAll});
+    localStorage.setItem("recipsList", JSON.stringify(recipeList));
+    modalEl.style.display = 'none';
+    planEl.style.display = 'none';
+    decriptionInputVal = '';
+    nameRecipInputVal = '';
+    ingrediendsAll = [];
+    instructionAll = [];
+    nameRecipInput.value = '';
+    decriptionInput.value = '';
+    olEl.innerHTML = '';
+    ulEl.innerHTML = '';
+  }
+});
+
+
+
