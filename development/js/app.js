5acae76ac1fa43fbb1fedac7cadf9949181e8e3f
MieczyslawMilej <mieczyslaw.milej@gmail.com>
2020-03-26 13:17:50 +0100

    utworzenie Utworzenie okna modal z eventem na kliknięcie

--- development/js/app.js
+++ development/js/app.js
@@ -1,43 +1,43 @@
-const nameUser = document.querySelector('.icons');
-const form = document.querySelector('form');
-const inpText = document.querySelector('#value');
-const secForm = document.querySelector('.nav-welcome');
-const mainSect = document.querySelector('.main-section');
+const nameUser = document.querySelector(".icons");
+const form = document.querySelector("form");
+const inpText = document.querySelector("#value");
+const secForm = document.querySelector(".nav-welcome");
+const mainSect = document.querySelector(".main-section");
 console.log(mainSect);
 
-form.addEventListener('submit', function (elem) {
-    elem.preventDefault();
-        nameUser.innerText = inpText.value;
-        var userName = inpText.value;
-        localStorage.setItem('savedName', userName);
-        console.log("Twoje imie zapisane w LS to:  ", localStorage.savedName);
-        secForm.style.display = 'none';
-        mainSect.style.display = 'block';
-
+form.addEventListener("submit", function(elem) {
+  elem.preventDefault();
+  nameUser.innerText = inpText.value;
+  var userName = inpText.value;
+  localStorage.setItem("savedName", userName);
+  console.log("Twoje imie zapisane w LS to:  ", localStorage.savedName);
+  secForm.style.display = "none";
+  mainSect.style.display = "block";
 });
 
-if(localStorage.length > 0){
-    secForm.style.display = 'none';
-    nameUser.innerText = localStorage.savedName;
-    mainSect.style.display = 'block';
-
+if (localStorage.length > 0) {
+  secForm.style.display = "none";
+  nameUser.innerText = localStorage.savedName;
+  mainSect.style.display = "block";
 }
 // Show modal //
 
-  const bodyEl = document.querySelector("body");
+const btnAddRecipie = document.querySelector("#add-reciepes");
+const btnAddPlan = document.querySelector("#add-plan");
 
-  const modalEl = document.createElement("div");
-  modalEl.classList.add("fullScreen");
+const modalEl = document.querySelector(".modal-window");
+const planEl = document.querySelector(".add-recipes-section");
 
 
+btnAddRecipie.addEventListener("click", function() {
+  modalEl.style.display = "flex";
+  planEl.style.display = "block";
 
-    btn.addEventListener("click", function() {
+  console.log("click");
+});
 
-      modalEl.style.display = 'block';
-newImg.setAttribute("src", srcAtr );
-      console.log(srcAtr);
-    });
-
-    newBtn.addEventListener('click', function() {
-    modalEl.parentElement.removeChild(modalEl);
+btnAddPlan.addEventListener("click", function() {
+  modalEl.style.display = "flex";
+  // dodać element który będzie ustawiał display block dla elementu plan //
+  console.log("click");
 });
