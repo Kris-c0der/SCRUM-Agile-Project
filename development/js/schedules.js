fa0d040c2816384781259f472870b9818f1bbbd0
Hubert-Kujawiak <h.kujawiak@hotmail.com>
2020-03-28 19:06:28 +0100

    wykonane

--- development/js/schedules.js
+++ development/js/schedules.js
@@ -4,29 +4,33 @@ if(localStorage.savedName != null){
     nameUser.innerText = localStorage.savedName;
 }
 
-// let dataTab = JSON.parse(localStorage.getItem('recipsList'));
-// console.log(dataTab);
-// const allRecipe = document.querySelector('.all-recipes');
-//
-// for (let i = 0; i < dataTab.length; i++) {
-//     const newDiv = document.createElement('div');
-//     newDiv.className = 'recipes';
-//
-//     const pId = document.createElement('p');
-//     pId.className = 'recipes-id';
-//
-//     const pName = document.createElement('p');
-//     pName.className = 'recipes-name';
-//
-//     const pDesc = document.createElement('p');
-//     pDesc.className = 'recipes-description';
-//
-//     const pAction = document.createElement('p');
-//     pAction.className = 'recipes-action';
-//
-//     allRecipe.appendChild(newDiv);
-//     newDiv.appendChild(pId).innerText = i + 1;
-//     newDiv.appendChild(pName).innerText = dataTab[i].nameRecipe;
-//     newDiv.appendChild(pDesc).innerText = dataTab[i].description;
-//     newDiv.appendChild(pAction);
-// }
+let planTab = JSON.parse(localStorage.getItem('planSchedule'));
+console.log(planTab);
+const allSchedule = document.querySelector('.all-schedules');
+
+for (let i = 0; i < planTab.length; i++) {
+    const newDiv = document.createElement('div');
+    newDiv.className = 'schedules';
+
+    const pId = document.createElement('p');
+    pId.className = 'schedules-id';
+
+    const pName = document.createElement('p');
+    pName.className = 'schedules-name';
+
+    const pDesc = document.createElement('p');
+    pDesc.className = 'schedules-description';
+
+    const pWeek = document.createElement('p');
+    pWeek.className = 'schedules-week';
+
+    const pAction = document.createElement('p');
+    pAction.className = 'schedules-action';
+
+    allSchedule.appendChild(newDiv);
+    newDiv.appendChild(pId).innerText = i + 1;
+    newDiv.appendChild(pName).innerText = planTab[i].namePlan;
+    newDiv.appendChild(pDesc).innerText = planTab[i].descriptionPlan;
+    newDiv.appendChild(pWeek).innerText = planTab[i].planWeek.length;
+    newDiv.appendChild(pAction);
+}
