3ec1bbc39b252c0a91e722133de07e46950b5d36
Krzysztof Kozak <hithard5@gmail.com>
2020-03-28 22:47:07 +0100

    Uzyskanie numeru tygodnia

--- development/js/app.js
+++ development/js/app.js
@@ -190,7 +190,6 @@ select.forEach(i => {
   }
 });
 
-
 /*tworzenie nowego planu*/
 
 createPlanForm.addEventListener("submit", function(e) {
@@ -280,8 +279,52 @@ createPlanForm.addEventListener("submit", function(e) {
 });
 // WYSWIETLANIE LICZBY PRZEPISOW W APP WIDGET
 
-const numRec = document.querySelector('.num-Rec');
-let numbersOfRecipe = JSON.parse(localStorage.getItem('recipsList'));
-if(localStorage.recipsList != null){
+const numRec = document.querySelector(".num-Rec");
+let numbersOfRecipe = JSON.parse(localStorage.getItem("recipsList"));
+if (localStorage.recipsList != null) {
   numRec.innerText = numbersOfRecipe.length;
 }
+
+// Wyświetlanie planów w ekranie głównym aplikacji.
+
+const months = [
+  "Styczeń",
+  "Luty",
+  "Marzec",
+  "Kwiecień",
+  "Maj",
+  "Czerwiec",
+  "Lipiec",
+  "Sierpień",
+  "Wrzesień",
+  "Październik",
+  "Listopad",
+  "Grudzień"
+];
+
+const currentDate = new Date(); // pełna data
+const currentMonth = currentDate.getMonth() + 1; //miesiąc || +1 ponieważ JS liczy miesiące od 0.
+const currentDay = currentDate.getDate(); //dzień miesiąca
+// console.log(months[currentMonth - 1]); // uzyskanie nazwy miesiąca zamiast liczby || -1 ponieważ pierwszy miesiąc ma indeks 0.
+
+
+// Uzyskanie numeru tygodnia w roku || źródło https://weeknumber.net/how-to/javascript
+Date.prototype.getWeek = function() {
+  var date = new Date(this.getTime());
+  date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return (
+    1 +
+    Math.round(
+      ((date.getTime() - week1.getTime()) / 86400000 -
+        3 +
+        ((week1.getDay() + 6) % 7)) /
+        7
+    )
+  );
+};
+
