a3f57041adb1ed4b70decc072314020aff90ee11
MieczyslawMilej <mieczyslaw.milej@gmail.com>
2020-03-28 18:20:20 +0100

    dodanie logiki do dodawania nowego planu

--- development/js/app.js
+++ development/js/app.js
@@ -4,10 +4,10 @@ const inpText = document.querySelector("#value");
 const secForm = document.querySelector(".nav-welcome");
 const mainSect = document.querySelector(".main-section");
 
-if(localStorage.savedName != null){
-    nameUser.innerText = localStorage.savedName;
-    secForm.style.display = "none";
-    mainSect.style.display = "block";
+if (localStorage.savedName != null) {
+  nameUser.innerText = localStorage.savedName;
+  secForm.style.display = "none";
+  mainSect.style.display = "block";
 }
 form.addEventListener("submit", function(elem) {
   elem.preventDefault();
@@ -53,97 +53,100 @@ window.onclick = function(event) {
 };
 
 ///////////////////*New Recipes - Logic*///////////////////////
-const btnElIns = document.querySelector('.instruction-btn');
-const textAreaInst = document.querySelector('#instruction-input');
-const olEl = document.querySelector('.instruction-list');
+const btnElIns = document.querySelector(".instruction-btn");
+const textAreaInst = document.querySelector("#instruction-input");
+const olEl = document.querySelector(".instruction-list");
 let instructionAll = [];
 
 //Event dodanie instrukcji
-btnElIns.addEventListener('click',function (e) {
+btnElIns.addEventListener("click", function(e) {
   e.preventDefault();
-  const textAreaInstValue = document.querySelector('#instruction-input').value;
-  if (textAreaInstValue.length < 150 && textAreaInstValue.length >0) {
-    const newLi = document.createElement('li');
-    newLi.classList.add('liIconsEl');
+  const textAreaInstValue = document.querySelector("#instruction-input").value;
+  if (textAreaInstValue.length < 150 && textAreaInstValue.length > 0) {
+    const newLi = document.createElement("li");
+    newLi.classList.add("liIconsEl");
     olEl.appendChild(newLi);
     newLi.innerText = textAreaInstValue;
-    const lielement = document.querySelector('.instruction-list li');
+    const lielement = document.querySelector(".instruction-list li");
     if (lielement !== null) {
-      const NewSpanEl = document.createElement('span');
-      const NewSpanSecEl = document.createElement('span');
-      NewSpanEl.classList.add('span-icon');
-      NewSpanSecEl.classList.add('span-delete-icon');
+      const NewSpanEl = document.createElement("span");
+      const NewSpanSecEl = document.createElement("span");
+      NewSpanEl.classList.add("span-icon");
+      NewSpanSecEl.classList.add("span-delete-icon");
       newLi.appendChild(NewSpanEl);
       newLi.appendChild(NewSpanSecEl);
-      NewSpanSecEl.addEventListener('click',function () {
-        console.log('click');
+      NewSpanSecEl.addEventListener("click", function() {
+        console.log("click");
         newLi.remove();
       });
     }
     instructionAll.push(textAreaInstValue);
-    textAreaInst.value = '';
+    textAreaInst.value = "";
   } else if (textAreaInstValue.length === 0) {
-    alert('Wprowadź instrukcję');
+    alert("Wprowadź instrukcję");
   } else {
-    alert('Maksymalna liczba znaków: 150 - Skróć instrukcję');
+    alert("Maksymalna liczba znaków: 150 - Skróć instrukcję");
   }
 });
 
-
 //Event dodanie składnika
-const btnElIng = document.querySelector('.ingredients-btn');
-const ulEl = document.querySelector('.ingredients-list');
-const inputIng = document.querySelector('#ingredients-input');
+const btnElIng = document.querySelector(".ingredients-btn");
+const ulEl = document.querySelector(".ingredients-list");
+const inputIng = document.querySelector("#ingredients-input");
 let ingrediendsAll = [];
 
-btnElIng.addEventListener('click',function (e) {
+btnElIng.addEventListener("click", function(e) {
   e.preventDefault();
-  const inputIngValue = document.querySelector('#ingredients-input').value;
-  if (inputIngValue.length < 50 && inputIngValue.length >0) {
-    const newLi2 = document.createElement('li');
+  const inputIngValue = document.querySelector("#ingredients-input").value;
+  if (inputIngValue.length < 50 && inputIngValue.length > 0) {
+    const newLi2 = document.createElement("li");
     ulEl.appendChild(newLi2);
     newLi2.innerText = inputIngValue;
-    const lielement2 = document.querySelector('.ingredients-list li');
+    const lielement2 = document.querySelector(".ingredients-list li");
     if (lielement2 !== null) {
-      const NewSpanEl2 = document.createElement('span');
-      const NewSpanSecEl2 = document.createElement('span');
-      NewSpanEl2.classList.add('span-icon');
-      NewSpanSecEl2.classList.add('span-delete-icon');
+      const NewSpanEl2 = document.createElement("span");
+      const NewSpanSecEl2 = document.createElement("span");
+      NewSpanEl2.classList.add("span-icon");
+      NewSpanSecEl2.classList.add("span-delete-icon");
       newLi2.appendChild(NewSpanEl2);
       newLi2.appendChild(NewSpanSecEl2);
-      NewSpanSecEl2.addEventListener('click',function () {
-        console.log('click');
+      NewSpanSecEl2.addEventListener("click", function() {
+        console.log("click");
         newLi2.remove();
       });
     }
     ingrediendsAll.push(inputIngValue);
-    inputIng.value ='';
+    inputIng.value = "";
   } else if (inputIngValue.length === 0) {
-    alert('Wprowadź nazwę składnika');
+    alert("Wprowadź nazwę składnika");
   } else {
-    alert('Maksymalna liczba znaków: 50 - Skróć nazwę składnika')
+    alert("Maksymalna liczba znaków: 50 - Skróć nazwę składnika");
   }
 });
 //Nazwa i opis przepisu
-const nameRecipInput = document.querySelector('#recipe-name');
-const decriptionInput = document.querySelector('#recipe-description');
-const saveBtnEl = document.querySelector('#save-recipes-btn');
-const formEl = document.querySelector('#form-add-recipe');
+const nameRecipInput = document.querySelector("#recipe-name");
+const decriptionInput = document.querySelector("#recipe-description");
+const saveBtnEl = document.querySelector("#save-recipes-btn");
+const formEl = document.querySelector("#form-add-recipe");
 
-formEl.addEventListener('submit', function (element) {
-  let decriptionInputVal = document.querySelector('#recipe-description').value;
-  let nameRecipInputVal = document.querySelector('#recipe-name').value;
-  if (nameRecipInputVal.length > 50 ) {
+formEl.addEventListener("submit", function(element) {
+  let decriptionInputVal = document.querySelector("#recipe-description").value;
+  let nameRecipInputVal = document.querySelector("#recipe-name").value;
+  if (nameRecipInputVal.length > 50) {
     element.preventDefault();
-    alert('Zbyt duża liczba znaków. Maksymlana liczba znaków dla "Nazwa przepisu": 50')
-  }else if (nameRecipInputVal.length === 0) {
-    alert('Nazwa przepisu musi zostać uzupełniona');
-  }else if (decriptionInputVal.length === 0) {
-    alert('Opis przepisu musi zostać uzupełniony');
-  }else if (decriptionInputVal.length >360) {
-    alert('Zbyt duża liczba znaków. Maksymlana liczba znaków dla "Opis przepisu": 360')
-  }else {
-    alert('Przepis wprowadzony prawidłowo');
+    alert(
+      'Zbyt duża liczba znaków. Maksymlana liczba znaków dla "Nazwa przepisu": 50'
+    );
+  } else if (nameRecipInputVal.length === 0) {
+    alert("Nazwa przepisu musi zostać uzupełniona");
+  } else if (decriptionInputVal.length === 0) {
+    alert("Opis przepisu musi zostać uzupełniony");
+  } else if (decriptionInputVal.length > 360) {
+    alert(
+      'Zbyt duża liczba znaków. Maksymlana liczba znaków dla "Opis przepisu": 360'
+    );
+  } else {
+    alert("Przepis wprowadzony prawidłowo");
     let recipeList = JSON.parse(localStorage.getItem("recipsList"));
     if (recipeList === null) {
       recipeList = [];
@@ -155,17 +158,16 @@ formEl.addEventListener('submit', function (element) {
       ingrediends: ingrediendsAll
     });
     localStorage.setItem("recipsList", JSON.stringify(recipeList));
-    modalEl.style.display = 'none';
-    planEl.style.display = 'none';
-    decriptionInputVal = '';
-    nameRecipInputVal = '';
+    modalEl.style.display = "none";
+    planEl.style.display = "none";
+    decriptionInputVal = "";
+    nameRecipInputVal = "";
     ingrediendsAll = [];
     instructionAll = [];
-    nameRecipInput.value = '';
-    decriptionInput.value = '';
-    olEl.innerHTML = '';
-    ulEl.innerHTML = '';
-
+    nameRecipInput.value = "";
+    decriptionInput.value = "";
+    olEl.innerHTML = "";
+    ulEl.innerHTML = "";
   }
 });
 
@@ -188,85 +190,93 @@ select.forEach(i => {
   }
 });
 
-console.log(recipeList);
 
 /*tworzenie nowego planu*/
 
-createPlanForm.addEventListener("submit", function(element) {
+createPlanForm.addEventListener("submit", function(e) {
   const namePlanInput = document.querySelector("#plan-name");
   const decriptionPlanInput = document.querySelector("#description");
-  const weekNumberVal = document.querySelector("#week-number").value;
+  let weekNumberVal = document.querySelector("#week-number").value;
 
   let namePlanInputVal = document.querySelector("#plan-name").value;
   let decriptionPlanInputVal = document.querySelector("#description").value;
 
-  if (decriptionPlanInputVal.length > 50 && namePlanInputVal.length > 360) {
-    element.preventDefault();
+  if (namePlanInputVal.length > 50) {
+    e.preventDefault();
     alert(
-      'Zbyt duża liczba znaków. Maksymlana liczba znaków dla "Nazwa Planu": 50, dla "Opis Planu": 360.'
+      'Zbyt duża liczba znaków. Maksymlana liczba znaków dla "Nazwa przepisu": 50'
+    );
+  } else if (namePlanInputVal.length === 0) {
+    alert("Nazwa planu musi zostać uzupełniona");
+  } else if (decriptionPlanInputVal.length === 0) {
+    alert("Opis planu musi zostać uzupełniony");
+  } else if (decriptionPlanInputVal.length > 360) {
+    alert(
+      'Zbyt duża liczba znaków. Maksymlana liczba znaków dla "Opis planu": 360'
     );
   } else {
-    if (allDays === null) {
-      allDays = [];
+    alert("Przepis wprowadzony prawidłowo");
+    let planSchedule = JSON.parse(localStorage.getItem("planSchedule"));
+    if (planSchedule === null) {
+      planSchedule = [];
     }
-
-    const monday = [];
-    const tuesday = [];
-    const wednesday = [];
-    const thursday = [];
-    const friday = [];
-    const saturday = [];
-    const sunday = [];
-
     /* dodawanie value z selectów do tablicy */
 
     const planName = document.querySelector("#plan-name");
     const description = document.querySelector("#description");
 
-    function getSelectedItems() {
-      const mondayElList = document.getElementsByClassName("monday-select");
-      const tuesdayElList = document.getElementsByClassName("tuesday-select");
-      const wednesdayElList = document.getElementsByClassName("wednesday-select");
-      const thursdayElList = document.getElementsByClassName("thursday-select");
-      const fridayElList = document.getElementsByClassName("friday-select");
-      const saturdayElList = document.getElementsByClassName("saturday-select");
-      const sundayElList = document.getElementsByClassName("sunday-select");
-      const week =[];
+    const mondayElList = document.getElementsByClassName("monday-select");
+    const tuesdayElList = document.getElementsByClassName("tuesday-select");
+    const wednesdayElList = document.getElementsByClassName("wednesday-select");
+    const thursdayElList = document.getElementsByClassName("thursday-select");
+    const fridayElList = document.getElementsByClassName("friday-select");
+    const saturdayElList = document.getElementsByClassName("saturday-select");
+    const sundayElList = document.getElementsByClassName("sunday-select");
+    const week = [];
 
-      week.push(mondayElList, tuesdayElList, wednesdayElList, thursdayElList, saturdayElList, sundayElList );
-      console.log(week);
+    week.push(
+      mondayElList,
+      tuesdayElList,
+      wednesdayElList,
+      thursdayElList,
+      fridayElList,
+      saturdayElList,
+      sundayElList
+    );
 
-// iteracja przez 1 tablice (monday), zmienić na iteracje przez wszystkie tablice w tablicy week, wymyślić jak zapisać dane z każdego tygodnia do jednej tablicy
+    /* pobieranie zaznaczonych wartości z inputów select i dodawanie ich do tablicy */
 
-      for (let i = 0; i < mondayElList.length; i++) {
-        let element = mondayElList[i];
-        let strSel = element.options[element.selectedIndex].text;
-        monday.push(strSel);
+    const planWeek = [];
+    const arr = [];
+    for (let i = 0; i < week.length; i++) {
+      arr[i] = [];
+      let element = week[i];
+      for (let j = 0; j < element.length; j++) {
+        let strSel = element[j].options[element[j].selectedIndex].text;
+        arr[i].push(strSel);
       }
-      console.log(monday);
     }
-    week.push()
 
-    getSelectedItems();
+    planWeek.push(arr);
 
-    allDays.push({
+    let idRecipe = planSchedule.length;
+    idRecipe++;
+
+    planSchedule.push({
       namePlan: namePlanInputVal,
       descriptionPlan: decriptionPlanInputVal,
-      allDays: allDays,
+      planWeek: planWeek,
       idRecipe: idRecipe,
       weekNumber: weekNumberVal
     });
-  //
-  //   localStorage.setItem("recipsList", JSON.stringify(recipeList));
-  //   modalEl.style.display = "none";
-  //   planEl.style.display = "none";
-  //   decriptionPlanInputVal = "";
-  //   namePlanInputVal = "";
-  //   ingrediendsAll = [];
-  //   instructionAll = [];
-  //   nameRecipInput.value = "";
-  //   decriptionInput.value = "";
-  //   olEl.innerHTML = "";
-  //   ulEl.innerHTML = "";
+
+    localStorage.setItem("planSchedule", JSON.stringify(planSchedule));
+    modalEl.style.display = "none";
+    planEl.style.display = "none";
+    decriptionPlanInputVal = "";
+    namePlanInputVal = "";
+    weekNumberVal = "";
   }
 });
+
+// console.warn("koniec kodu");
